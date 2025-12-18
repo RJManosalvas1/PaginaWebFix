dbff5473c68a82555e64a7b36425b978760c0a63
RJManosalvas1 <roberto.manosalvas@udla.edu.ec>
2025-12-18 00:47:26 -0500

    SUNMI PAGE CHANGES

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
-	theme: {
-		fontFamily: {
-			sans: ['Exo, sans-serif']
-		},
-			extend: {
-				colors: {	customBlack: '#0c0a09',
-				customOrange :'#ea580c',
-				customBlue: '#0c4a6e',
-				  },
-			}
-		},
-	variants:{},
-	plugins: [],
-}
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Exo", "sans-serif"],
+      },
+      fontSize: {
+        base: "20px",
+        lg: "20px",
+        xl: "24px",
+        "2xl": "32px",
+      },
+      colors: {
+        customBlack: "#0c0a09",
+        customOrange: "#ea580c",
+        customBlue: "#0c4a6e",
+      },
+    },
+  },
+
+  plugins: [],
+};
